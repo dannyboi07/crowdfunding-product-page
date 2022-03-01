20352c89fd0e27c3596b3824b7cc3132f0efcb1a
dannyboi07 <48730475+dannyboi07@users.noreply.github.com>
2022-03-02 00:41:01 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const bmark = document.getElementById("bmark");
+
+bmark.addEventListener("click", () => {
+  const bmarkImg = document.getElementById("bmarkImg");
+  bmarkImg.classList.toggle("section-1__bmark-ctn__bmark-img--green");
+
+  bmark.classList.toggle("section-1__bmark--green-text");
+  bmark.classList.contains("section-1__bmark--green-text")
+  ? bmark.textContent = "Bookmarked"
+  : bmark.textContent = "Bookmark"
+
+})
