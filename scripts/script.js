3e64d3fe753030aec665cf298673e3808de7ada9
dannyboi07 <48730475+dannyboi07@users.noreply.github.com>
2022-03-06 01:16:04 +0530

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 const bmark = document.getElementById("bmark");
+const ulRwds = document.getElementById("pldg-rwd-list");
+const rwdInputs = ulRwds.getElementsByTagName("input");
+// Array.from(rwdInputs).forEach(e, i => console.log(e, i));
 
 bmark.addEventListener("click", () => {
   const bmarkImg = document.getElementById("bmarkImg");
@@ -10,3 +13,40 @@ bmark.addEventListener("click", () => {
   : bmark.textContent = "Bookmark"
 
 });
+
+for (let i = 1; i <= 4; i++) {
+  const pldgItem = document.getElementById(`pldg-rwd-item-${i}`);
+  const rwdInput = document.getElementById(`pledge-rwd-${i}`);
+  const hdr = document.getElementById(`pldg-hdr-${i}`);
+
+  hdr.addEventListener("click", () => {
+    const pldgItems = ulRwds.getElementsByTagName("li");
+    rwdInput.checked = true;
+    Array.from(pldgItems).forEach(pldgLiItem => {
+      if (pldgLiItem.classList.contains("pldg-rwd-item--active")) {
+        pldgLiItem.classList.remove("pldg-rwd-item--active");
+      }
+    })
+    pldgItem.classList.add("pldg-rwd-item--active");
+  });
+
+  hdr.addEventListener("mouseover", () => {
+    rwdInput.style.borderColor = "hsl(176, 50%, 47%)";
+  });
+
+  hdr.addEventListener("mouseout", () => {
+    rwdInput.style.borderColor = "hsl(0, 0%, 75%)";
+  })
+}
+
+// for (let i = 0; i < 4; i++) {
+//   rwdInputs[i].addEventListener("click", () => {
+//     for (let j = 0; j < 4; j++) {
+//       if (rwdInputs[j].checked) {
+//         rwdInputs[j].checked = false;
+//       };
+//     };
+
+//     rwdInputs[i].checked = true;
+//   });
+// };
