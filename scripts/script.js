0322d812a1959a53df5743d18b17080d9194064f
dannyboi07 <48730475+dannyboi07@users.noreply.github.com>
2022-03-07 01:34:37 +0530

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 const bmark = document.getElementById("bmark");
 const ulRwds = document.getElementById("pldg-rwd-list");
 const rwdInputs = ulRwds.getElementsByTagName("input");
-// Array.from(rwdInputs).forEach(e, i => console.log(e, i));
+const bckProjBtn = document.querySelector(".section-1__back");
+const bckProjMdlCls = document.querySelector(".back-prj-mdl__cls-mdl");
 
 bmark.addEventListener("click", () => {
   const bmarkImg = document.getElementById("bmarkImg");
@@ -11,23 +12,71 @@ bmark.addEventListener("click", () => {
   bmark.classList.contains("section-1__bmark--green-text")
   ? bmark.textContent = "Bookmarked"
   : bmark.textContent = "Bookmark"
-
 });
 
-for (let i = 1; i <= 4; i++) {
+bckProjBtn.addEventListener("click", () => {
+  const bckProjMdl = document.querySelector(".back-prj-mdl");
+  const greyBg = document.querySelector(".bck-prj-mdl-bg");
+  greyBg.classList.add("bck-prj-mdl-bg--show");
+  
+  setTimeout(() => {
+    bckProjMdl.style.display = "block";
+
+    setTimeout(() => {
+      bckProjMdl.classList.add("back-prj-mdl--show");
+    }, 15);
+  }, 0);
+});
+
+bckProjMdlCls.addEventListener("click", () => {
+  const bckProjMdl = document.querySelector(".back-prj-mdl");
+  const greyBg = document.querySelector(".bck-prj-mdl-bg");
+  greyBg.classList.remove("bck-prj-mdl-bg--show");
+
+  bckProjMdl.classList.remove("back-prj-mdl--show");
+  setTimeout(() => {
+    bckProjMdl.style.display = "none";
+  }, 100);
+})
+
+for (let i = 1; i <= 3; i++) {
   const pldgItem = document.getElementById(`pldg-rwd-item-${i}`);
   const rwdInput = document.getElementById(`pledge-rwd-${i}`);
   const hdr = document.getElementById(`pldg-hdr-${i}`);
+  const pldgBtm = document.getElementById(`pldg-btm-${i}`);
+  let timer = null;
 
   hdr.addEventListener("click", () => {
+    if (timer) clearTimeout(timer);
+
     const pldgItems = ulRwds.getElementsByTagName("li");
     rwdInput.checked = true;
-    Array.from(pldgItems).forEach(pldgLiItem => {
+
+    Array.from(pldgItems).forEach((pldgLiItem, j) => {
       if (pldgLiItem.classList.contains("pldg-rwd-item--active")) {
         pldgLiItem.classList.remove("pldg-rwd-item--active");
+
+        const pldgLiItemBtm = pldgLiItem.querySelector(".pldg-rwd-item__bottom.pldg-rwd-item__bottom--show");
+        if (j+1 !== i) pldgLiItemBtm.classList.remove("pldg-rwd-item__bottom--show");
       }
-    })
-    pldgItem.classList.add("pldg-rwd-item--active");
+      else if (pldgLiItem.classList.contains("pldg-rwd-item--active-mod")) {
+        pldgLiItem.classList.remove("pldg-rwd-item--active-mod");
+      }
+    });
+
+    if (i !== 1) {
+      pldgItem.classList.add("pldg-rwd-item--active");
+      timer = setTimeout(() => {
+        pldgBtm.style.display = "flex";
+  
+        setTimeout(() => {
+          pldgBtm.classList.add("pldg-rwd-item__bottom--show")        
+        }, 10);
+      }, 0);
+    }
+    else {
+      pldgItem.classList.add("pldg-rwd-item--active-mod");
+    }
   });
 
   hdr.addEventListener("mouseover", () => {
@@ -35,9 +84,21 @@ for (let i = 1; i <= 4; i++) {
   });
 
   hdr.addEventListener("mouseout", () => {
-    rwdInput.style.borderColor = "hsl(0, 0%, 75%)";
-  })
-}
+    rwdInput.style = undefined;
+  });
+
+  rwdInput.addEventListener("mouseover", () => {
+    hdr.style.color = "hsl(176, 50%, 47%)";
+  });
+
+  rwdInput.addEventListener("mouseout", () => {
+    hdr.style = undefined;
+  });
+
+  rwdInput.addEventListener("click", () => {
+    hdr.click();
+  });
+};
 
 // for (let i = 0; i < 4; i++) {
 //   rwdInputs[i].addEventListener("click", () => {
